f99690dddfc561a932c5c925f0051e9db9933315
Vinther901 <jv97@live.dk>
2020-07-09 13:55:52 +0200

    2nd attempt at type classifying, still not succesful

--- Models/__init__.py
+++ Models/__init__.py
@@ -1,4 +1,5 @@
 #Model1: First try
-#Model2: First selfmade
+#Model2: First selfmade, now used for energy prediction
 #Model3: Model2 readjusted for classifying types
 #Model4: Improving Model3, perhaps total do over.
+#Model5: First attempt at DNAConv for type prediction
